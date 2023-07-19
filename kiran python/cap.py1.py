7a44198db7f98b9dadfa069a976056d53e34ea2b
kiran subba <klimboo85@gmail.com>
2023-07-19 10:19:42 -0700

    pushing files to the repo

---
+++ kiran python/cap.py1.py
@@ -0,0 +1,7 @@
+def cap_text(text):
+    '''     
+    input the string
+    output the capitalized string
+    '''
+    return text.title()
+    #.title
---
+++ simple1.py
@@ -0,0 +1,13 @@
+'''
+A vry simple script
+'''
+def myfunction():
+    '''
+    A simple function 
+    '''
+    # printing and declaring the numbers
+    first_num = 1
+    second_num = 2
+    print(first_num)
+    print(second_num)
+myfunction()
